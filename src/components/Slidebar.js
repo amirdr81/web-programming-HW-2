11a41f86a77dea1f586f2c1491fe9648919bc0be
amirdr81 <amirmohammadd@yahoo.com>
2025-07-22 17:37:03 +0330

    drag and drop shapes updated

--- src/components/Slidebar.js
+++ src/components/Slidebar.js
@@ -7,10 +7,10 @@ function Slidebar({onDragStart}) {
       <div className="shape-container">
         <h1 className="title">Tools</h1>
         <div className="shape shape-circle" draggable onDragStart={(e) => onDragStart(e, 'circle')}></div>
-        <div className="shape shape-square"></div>
-        <div className="shape shape-triangle"></div>
-        <div className="shape shape-ellipse"></div>
-        <div className="shape shape-parallelogram"></div>
+        <div className="shape shape-square" draggable onDragStart={(e) => onDragStart(e, 'square')}></div>
+        <div className="shape shape-triangle" draggable onDragStart={(e) => onDragStart(e, 'triangle')}></div>
+        <div className="shape shape-ellipse" draggable onDragStart={(e) => onDragStart(e, 'ellipse')}></div>
+        <div className="shape shape-parallelogram" draggable onDragStart={(e) => onDragStart(e, 'parallelogram')}></div>
       </div>
     </div>
   );
