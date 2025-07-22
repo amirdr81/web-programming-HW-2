fc77dc22b6982f9010c54a36843019f4c9d069f9
amirdr81 <amirmohammadd@yahoo.com>
2025-07-22 16:42:22 +0330

    counter box params added

--- src/components/Slidebar.js
+++ src/components/Slidebar.js
@@ -5,7 +5,7 @@ function Slidebar() {
   return (
     <div className="slidebar-box">
       <div className="shape-container">
-        <h1 className="shape title">Tools</h1>
+        <h1 className="title">Tools</h1>
         <div className="shape shape-circle"></div>
         <div className="shape shape-square"></div>
         <div className="shape shape-triangle"></div>
--- src/components/counter.js
+++ src/components/counter.js
@@ -4,7 +4,28 @@ import "./counter.css";
 function Counter() {
   return (
     <div className="counter-box">
-      Counter box
+      <div className="counter-shapes-row">
+        <div className="counter-shape-item">
+          <div className="shape shape-circle" />
+          <span className="counter-number">0</span>
+        </div>
+        <div className="counter-shape-item">
+          <div className="shape shape-triangle" />
+          <span className="counter-number">0</span>
+        </div>
+        <div className="counter-shape-item">
+          <div className="shape shape-square" />
+          <span className="counter-number">0</span>
+        </div>
+        <div className="counter-shape-item">
+          <div className="shape shape-parallelogram" />
+          <span className="counter-number">0</span>
+        </div>
+        <div className="counter-shape-item">
+          <div className="shape shape-ellipse" />
+          <span className="counter-number">0</span>
+        </div>
+      </div>
     </div>
   );
 }
