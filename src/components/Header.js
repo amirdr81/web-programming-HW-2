354dc2ba9b2609e5e20aef03999c5c2529934cab
amirdr81 <amirmohammadd@yahoo.com>
2025-07-22 16:12:35 +0330

    header and tools front updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,11 @@ import "./Header.css";
 function Header() {
   return (
     <div className="header-box">
-      header box
+      <div className="header-title">Painting Title</div>
+      <div className="btn-group">
+        <button className="btn">Export</button>
+        <button className="btn">Import</button>
+      </div>
     </div>
   );
 }
--- src/components/Slidebar.js
+++ src/components/Slidebar.js
@@ -4,7 +4,14 @@ import "./Slidebar.css";
 function Slidebar() {
   return (
     <div className="slidebar-box">
-      Slidebar box
+      <div className="shape-container">
+        <h1 className="shape title">Tools</h1>
+        <div className="shape shape-circle"></div>
+        <div className="shape shape-square"></div>
+        <div className="shape shape-triangle"></div>
+        <div className="shape shape-ellipse"></div>
+        <div className="shape shape-parallelogram"></div>
+      </div>
     </div>
   );
 }
